06883f21f28cca33a0102db3fe8eae9d2b4cc47e
Michał Szałański <michal.szalanski1@gmail.com>
2020-07-05 20:42:59 +0200

    Modify testing code to include the task

--- application/a-01.R
+++ application/a-01.R
@@ -6,18 +6,19 @@
 library(tidyverse)
 library(ggplot2)
 
-# 1. remove package if it exists
+# remove package if it exists
+# Restarting R session still necesarry
 remove.packages("priceThisOption")
 detach("package:priceThisOption", unload = TRUE) # if it still is in memory
 
 # 2. install package and load to memory
-#install.packages("../priceThisOption_0.1.0.tar.gz",
-#                 type = "source",
-#                 repos = NULL)
-
-install.packages("../priceThisOption_0.1.0.zip",
-                 type = "binary",
+install.packages("../priceThisOption_0.1.0.tar.gz",
+                 type = "source",
                  repos = NULL)
+
+#install.packages("../priceThisOption_0.1.0.zip",
+#                 type = "binary",
+#                 repos = NULL)
 # load to memory
 library("priceThisOption")
 
@@ -27,7 +28,7 @@ getGeometricAsianPrice(
   vol = 0.25,
   r = 0.07,
   t = 252,
-  nreps = 10000
+  nreps = 100000
 )
 
 
@@ -59,41 +60,55 @@ tibble(price, result) %>%
     y     = "price of European call option"
   )
 
-# 8. build an R function: option price vs. number of loops
-MCEuropeanCallLoops <- function(loops) {
-  return(
-    MCEuropeanOptionPricer(0.5, 100, 95, 0.2, 0.06, loops, 0)
+
+testVolatility <- function(vol){
+  getGeometricAsianPrice(
+    price = 75,
+    spread = 5,
+    vol = vol,
+    r = 0.07,
+    t = 252,
+    nreps = 10000
   )
 }
 
-MCEuropeanCallLoops(500)
-loops  <- 2 ^ (1:25)
-result <- sapply(loops, MCEuropeanCallLoops)
+vol  <- c(0.001, 0.01, 0.02, 0.05, 0.1, 0.15, 0.2, 0.3, 0.5, 1)
+result <- sapply(vol, testVolatility)
 
-tibble(loops, result) %>%
-  ggplot(aes(x = as.factor(loops), y = result)) +
-  geom_point() + 
+
+# 7. same plot using ggplot2 package
+tibble(vol, result) %>%
+  ggplot(aes(x = vol, y = result, col = "red")) +
+  geom_line() +
+  geom_point(size = 2, shape = 21, fill = "white") +
   labs(
-    x = "number of loops",
-    y = "price of European call option",
-    title = "price of European call option vs. number of loops") +
-  theme(axis.text.x = element_text(angle = 90))
+    title = "price of European call option vs. time  to maturity",
+    x     = "time to maturity",
+    y     = "price of European call option"
+  )
 
-# 9. build an R function: option price vs. spot and volatility
-MCEuropeanCallSpotVol <- function(spot, vol) {
-  return(
-    MCEuropeanOptionPricer(0.5, 100, spot, vol, 0.06, 100000, 0)
+
+
+testPriceVolatility <- function(price, vol){
+  getGeometricAsianPrice(
+    price = price,
+    spread = 5,
+    vol = vol,
+    r = 0.07,
+    t = 252,
+    nreps = 10000
   )
 }
-# call function once
-MCEuropeanCallSpotVol(100, 0.2)
+
+
+
 
 # sequences of argument values
 spot <- seq(90, 105, by = 0.5)
 vol  <- c(0.001, 0.01, 0.02, 0.05, 0.1, 0.15, 0.2, 0.3, 0.5, 1)
 
 grid <- expand.grid(spot = spot, vol = vol)
-result <- mapply(MCEuropeanCallSpotVol, spot = grid$spot, vol = grid$vol)
+result <- mapply(testPriceVolatility, grid$spot, grid$vol)
 result.df <- data.frame(grid, result)
 result.df
 
